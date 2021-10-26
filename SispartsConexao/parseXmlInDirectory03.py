3018516ad9d7b7d7d64cf501e3d5c7d3d0e5564d
Ailton Zacarias <ailtonzsilva@gmail.com>
2021-10-26 11:17:08 -0300

    20211026-1115 - qryCompras_Update_Dados - Implementação de campo ID_Forn_CompraNF

--- SispartsConexao/parseXmlInDirectory03.py
+++ SispartsConexao/parseXmlInDirectory03.py
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 import os
 from xml.dom.minidom import parse, parseString
 
-listDirectory = ["C:\\tools\\_Proparts\\Coleta\\68.365.5010002-96 - Proparts Comércio de Artigos Esportivos e Tecnologia Ltda\\"]
+listDirectory = ["C:\\temp\\68.365.5010002-96 - Proparts Comércio de Artigos Esportivos e Tecnologia Ltda\\recebimento\\"]
 for directory in (listDirectory):
     for filename in os.listdir(directory):
         if filename.endswith(".xml"):
@@ -20,27 +19,3 @@ for directory in (listDirectory):
 
                 except OSError as e:
                     print("#### " + e.filename)
-=======
-import os
-from xml.dom.minidom import parse, parseString
-
-listDirectory = ["C:\\tools\\_Proparts\\Coleta\\68.365.5010002-96 - Proparts Comércio de Artigos Esportivos e Tecnologia Ltda\\"]
-for directory in (listDirectory):
-    for filename in os.listdir(directory):
-        if filename.endswith(".xml"):
-            with open(os.path.join(directory, filename)) as xmldata:
-                try:
-
-                    xml = parseString(xmldata.read())
-                    xml_pretty_str = xml.toprettyxml()
-                    nomaArquivo = filename.split(".")[0] + ".txt"
-                    f = open(os.path.join(directory, nomaArquivo), "a+")
-                    f.write(xml_pretty_str)
-                    f.close
-
-                    print(filename.split(".")[0])
-
-                except OSError as e:
-                    print("#### " + e.filename)
->>>>>>> 1ca457995f00e2efcfc0b78954c7ab4cf96ff95c
-                    print(e)
---
+++ referencia/azs_VALIDAR_DADOS.bas
@@ -0,0 +1,8 @@
+Attribute VB_Name = "azs_VALIDAR_DADOS"
+Option Compare Database
+
+
+
+
+
+
