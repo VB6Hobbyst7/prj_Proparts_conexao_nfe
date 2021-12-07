eefe8afb894b624bcc1985a4d4080d6ed19290cd
Ailton Zacarias <ailtonzsilva@gmail.com>
2021-12-07 08:04:08 -0300

    20211207-0804

--- SispartsConexao/templates.sql
+++ SispartsConexao/templates.sql
@@ -1,5 +1,16 @@
+select 
+		ID_NatOper
+		,CFOP_NatOper
+		,Fil_NatOper 
+		,descr_natoper
+	-- SELECT COUNT(*)	-- SELECT * -- DELETE
+	from tblNatOp
+	where CFOP_NatOper='2.152'
+
 SELECT ChvAcesso_CompraNF
 	,NumPed_CompraNF
+	,ID_NatOp_CompraNF
+	,tblNatOp.descr_natoper
 	,CNPJ_CPF_CompraNF
 	,NomeCompleto_CompraNF
 	,NumNF_CompraNF
@@ -16,10 +27,13 @@ SELECT ChvAcesso_CompraNF
 	,IDVD_CompraNF
 -- SELECT ChvAcesso_CompraNF,NumPed_CompraNF,ID_Forn_CompraNF
 -- SELECT COUNT(*)	-- SELECT * -- DELETE
-FROM tblCompraNF
+FROM tblCompraNF INNER JOIN tblNatOp ON tblCompraNF.ID_NatOp_CompraNF = tblNatOp.ID_NatOper
+where ModeloDoc_CompraNF = 55 and ID_NatOp_CompraNF = 122;
 
--- where ChvAcesso_CompraNF = '32210204884082000569570000039547081039547081'
+
 SELECT Item_CompraNFItem
+	,ID_NatOp_CompraNFItem
+	,ID_Grade_CompraNFItem
 	,ID_Prod_CompraNFItem
 	,CFOP_CompraNFItem
 	,BaseCalcICMSSubsTrib_CompraNFItem
@@ -36,47 +50,86 @@ SELECT Item_CompraNFItem
 -- SELECT ID_CompraNFItem
 -- SELECT DISTINCT(ID_Prod_CompraNFItem)
 -- SELECT COUNT(*)	-- SELECT * -- DELETE
-FROM tblCompraNFItem;
-	
+FROM tblCompraNFItem INNER JOIN tblCompraNF ON tblCompraNF.ID_CompraNF = tblCompraNFItem.ID_CompraNF_CompraNFItem
+where tblCompraNF.ModeloDoc_CompraNF = 55 and tblCompraNF.ChvAcesso_CompraNF = '42210212680452000302550020000886301507884230'
 
-/*
+
+
+
+/*** CADASTRO DE NATUREZA DE OPERAÇÃO
+
+	select 
+		ID_NatOper
+		,CFOP_NatOper
+		,Fil_NatOper 
+		,descr_natoper
+	-- SELECT COUNT(*)	-- SELECT * -- DELETE
+	from tblNatOp
+	where CFOP_NatOper='2.152'
+	where CFOP_NatOper='1.907'
+	where ID_NatOper=419
+ 
+ */
+
+
+
+
+
+/*** CADASTRO DE CLIENTES
 	
-INSERT INTO tblCompraNF (
-	ChvAcesso_CompraNF
-	,CNPJ_CPF_CompraNF
-	,NomeCompleto_CompraNF
-	,NumPed_CompraNF
-	,NumNF_CompraNF
-	,DTEmi_CompraNF
-	,HoraEntd_CompraNF
-	,ModeloDoc_CompraNF
-	,Obs_CompraNF
-	,Serie_CompraNF
-	,TPNF_CompraNF
-	,BaseCalcICMS_CompraNF
-	,VTotICMS_CompraNF
-	,VTotIPI_CompraNF
-	,VTotNF_CompraNF
-	,IDVD_CompraNF
-	)
-SELECT '32210268365501000296550000000637741001351624'
-	,'68365501000296'
-	,'PROPARTS COM ART ESPORTIVOS E TECN EIRELI'
-	,(
-		SELECT max(IsNull(NumPed_CompraNF, 0)) + 1 AS contador
-		FROM tblCompraNF
+	SET IDENTITY_INSERT Clientes ON
+
+	INSERT INTO Clientes (CÃ“DIGOClientes,NomeCompleto,CNPJ_CPF,Estado,envia,opMTB,opAv,opTri,opEstr,opDH,opCami,opOut,OptSimples_Cad,vdRS,vdAV,vdSP,vdSC,vdSR,vdTR,vd661,FlagSel_Cad,FlagAtivo,FlagSemRest,FlagBloq,FlagMsgCob,FlagSitTransp_Cad) 
+	SELECT * FROM ( VALUES
+	(23306,'Proparts Com Art Esp e Tec - Filial Es','68.365.501/0002-96','ES',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+	,(46036,'Proparts Com Art Esp e Tec - Filial Sc','68.365.501/0003-77','SC',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+	,(1025,'Proparts Com Art Esp e Tec - Matriz Sp','68.365.501/0001-05','SP',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+	) AS tmp(str_CODIGOClientes,str_NomeCompleto,str_CNPJ_CPF,str_Estado,envia,opMTB,opAv,opTri,opEstr,opDH,opCami,opOut,OptSimples_Cad,vdRS,vdAV,vdSP,vdSC,vdSR,vdTR,vd661,FlagSel_Cad,FlagAtivo,FlagSemRest,FlagBloq,FlagMsgCob,FlagSitTransp_Cad);
+
+	SET IDENTITY_INSERT Clientes OFF
+	
+	
+	*/
+
+
+/*** CADASTRO DE COMPRAS ( SEM ITENS )
+	
+	INSERT INTO tblCompraNF (
+		ChvAcesso_CompraNF
+		,CNPJ_CPF_CompraNF
+		,NomeCompleto_CompraNF
+		,NumPed_CompraNF
+		,NumNF_CompraNF
+		,DTEmi_CompraNF
+		,HoraEntd_CompraNF
+		,ModeloDoc_CompraNF
+		,Obs_CompraNF
+		,Serie_CompraNF
+		,TPNF_CompraNF
+		,BaseCalcICMS_CompraNF
+		,VTotICMS_CompraNF
+		,VTotIPI_CompraNF
+		,VTotNF_CompraNF
+		,IDVD_CompraNF
 		)
-	,63774
-	,'2021/02/15'
-	,'1899/12/30 09:44:00'
-	,'55'
-	,'PEDIDO 322295    TICKET TRANSF;'
-	,'0'
-	,'1'
-	,4527.48
-	,181.10
-	,0
-	,4980.23
-	,'322295';	
+	SELECT '32210268365501000296550000000637741001351624'
+		,'68365501000296'
+		,'PROPARTS COM ART ESPORTIVOS E TECN EIRELI'
+		,(
+			SELECT max(IsNull(NumPed_CompraNF, 0)) + 1 AS contador
+			FROM tblCompraNF
+			)
+		,63774
+		,'2021/02/15'
+		,'1899/12/30 09:44:00'
+		,'55'
+		,'PEDIDO 322295    TICKET TRANSF;'
+		,'0'
+		,'1'
+		,4527.48
+		,181.10
+		,0
+		,4980.23
+		,'322295';	
 	
 */
